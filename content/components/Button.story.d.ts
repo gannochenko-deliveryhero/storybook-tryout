8e69f4d7847da55b221ce9bcdff2e837187c1474
Sergei Gannochenko <s.gannochenko@c02c180mmd6t.ad.deliveryhero.com>
2020-05-29 13:43:59 +0200

    wip

---
+++ content/components/Button.story.d.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+declare const _default: {
+    title: string;
+    component: React.FunctionComponent<import("@deliveryhero/armor/build.cjs/components/Button/type").ButtonPropsType>;
+    decorators: ((...args: any) => any)[];
+    parameters: {};
+};
+export default _default;
+export declare const Generic: () => JSX.Element;
---
+++ content/components/Message.story.d.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+declare const _default: {
+    title: string;
+    component: React.FunctionComponent<import("@deliveryhero/armor/build.cjs/components/Message/type").MessagePropsType>;
+    decorators: ((...args: any) => any)[];
+    parameters: {};
+};
+export default _default;
+export declare const Generic: () => JSX.Element;
